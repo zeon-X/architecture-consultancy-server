d44bf9bbb8473db07f2fb7f99cb9faf2caaa1e6d
Md. Shefat Zeon <mdshefatzeon@gmail.com>
2023-02-02 17:50:26 +0600

    done something

--- fileHandlers/fileHandle.js
+++ fileHandlers/fileHandle.js
@@ -1,30 +1,18 @@
 const router = require("express").Router();
 const fs = require("fs");
-const DESTFILE = "./public/data/file/";
-const DESTIMAGE = "./public/data/image/";
+const DESTFILE = "./uploads/";
 // responsing function
 const uploadFile = function (req, res) {
   if (!req.files) {
     return res.status(400).send({ msg: "bad request" });
   }
-  if (req.files.file == undefined) {
-    //IMAGE
-    fs.writeFile(DESTIMAGE + req.files.image.name, req.files.image.data, () => {
-      const url = req.protocol + "://" + req.get("host");
-      const finalURL = url + "/public/data/image/" + req.files.image.name;
-      console.log("image has been written");
 
-      return res.status(200).send({ url: finalURL });
-    });
-  } else {
-    //FILE
-    fs.writeFile(DESTFILE + req.files.file.name, req.files.file.data, () => {
-      const url = req.protocol + "://" + req.get("host");
-      const finalURL = url + "/public/data/file/" + req.files.file.name;
-      console.log("file has been written");
-      return res.status(200).send({ url: finalURL });
-    });
-  }
+  fs.writeFile(DESTFILE + req.files.file.name, req.files.file.data, () => {
+    const url = req.protocol + "://" + req.get("host");
+    const finalURL = url + "/public/" + req.files.file.name;
+    // console.log("file has been written");
+    return res.status(200).send({ url: finalURL });
+  });
 };
 
 // ROUTER -> upload file -> then CONTROLLER
--- index.js
+++ index.js
@@ -2,14 +2,15 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const fileUpload = require("express-fileupload");
+const path = require("path");
 const app = express();
 
 require("dotenv").config();
 app.use(cors());
 app.use(express.json());
 app.use(fileUpload());
-app.use("/public/data/file/", express.static("public"));
-app.use("/public/data/image/", express.static("public"));
+
+app.use("/public", express.static(path.join(__dirname, "uploads")));
 
 const PORT = process.env.PORT || 5000;
 
