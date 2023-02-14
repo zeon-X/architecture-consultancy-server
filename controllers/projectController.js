8a8e0f7ac1d72df89d343572ce1689a5b377d793
Md. Shefat Zeon <mdshefatzeon@gmail.com>
2023-02-14 15:57:42 +0600

    done something

--- controllers/projectController.js
+++ controllers/projectController.js
@@ -124,7 +124,15 @@ const getAllActiveProjectsByNameAndImage = async (req, res) => {
     let fproject;
 
     fproject = await Project.find({ status: "active" })
-      .select({ _id: 1, title: 1, img: 1, location: 1, aboutLeft: 1 })
+      .select({
+        _id: 1,
+        title: 1,
+        img: 1,
+        location: 1,
+        aboutLeft: 1,
+        category: 1,
+      })
+      .populate("category")
       .sort({ createdAt: -1 })
       .skip(qpage * qlimit)
       .limit(qlimit);
@@ -139,11 +147,16 @@ const getAllActiveProjectsByNameAndImage = async (req, res) => {
 const getProjectById = async (req, res) => {
   if (!req.query._id) res.status(500).json({ msg: "provide an project _id" });
   try {
-    let fproject = await Project.findById(req.query._id);
-    if (fproject.reviewId !== "") {
-      fproject = await Project.findById(req.query._id).populate("reviewId");
+    let fproject = await Project.findById(req.query._id).populate("category");
+
+    if (fproject?.reviewId !== "") {
+      fproject = await Project.findById(req.query._id).populate([
+        { path: "reviewId", model: "Review" },
+        { path: "category", model: "Category" },
+      ]);
     }
     // console.log(fproject);
+
     res.status(200).json(fproject);
   } catch (err) {
     res.status(500).json(err);
@@ -180,7 +193,15 @@ const getProjectByCategoryBasic = async (req, res) => {
       category: req?.query?._catId,
       status: "active",
     })
-      .select({ _id: 1, title: 1, img: 1, location: 1, aboutLeft: 1 })
+      .select({
+        _id: 1,
+        title: 1,
+        img: 1,
+        location: 1,
+        aboutLeft: 1,
+        category: 1,
+      })
+      .populate("category")
       .sort({ createdAt: -1 })
       .skip(qpage * qlimit)
       .limit(qlimit);
