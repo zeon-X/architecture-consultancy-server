ccfdf7f53262d9c3798debc2b91fc907a112d32e
Md. Shefat Zeon <mdshefatzeon@gmail.com>
2023-02-22 06:33:43 +0600

    done something

--- fileHandlers/fileHandle.js
+++ fileHandlers/fileHandle.js
@@ -6,18 +6,27 @@ const uploadFile = function (req, res) {
   if (!req.files) {
     return res.status(400).send({ msg: "bad request" });
   }
+
   // console.log(req?.user?._id);
 
+  //REGEX
+  // ([A-Z|a-z|0-9]\w*)|(\.|\-)
+  // (\s|[$&+,:;=?@#|'<>^*(){}[\]%!])
+  // fileName = fileName.replace(/\s+/g, "_").trim();
+
   let fileName = req.files.file.name;
-  fileName = fileName.replace(/\s+/g, "_").trim();
   fileName = fileName.toLowerCase();
-  fileName = req?.user?._id + "_id_" + fileName;
+  // fileName = fileName.replace(/\s*,\s*|\s+,/g, "_").trim();
+  fileName = fileName.replace(/(\s|[$&+,:;=?@#|'<>^*(){}[\]%!])/g, "_").trim();
+  let sliceId = req?.user?._id?.slice(0, 16) || "zzxxccvvbbnnmmllppooii";
+  fileName = sliceId + "_id_" + fileName;
   // console.log(fileName);
 
   fs.writeFile(DESTFILE + fileName, req.files.file.data, () => {
-    const url = req.protocol + "://" + req.get("host");
+    // const url = req.protocol + "://" + req.get("host");
+    const url = "https://" + req.get("host");
     const finalURL = url + "/public/" + fileName;
-    // console.log("file has been written");
+    // console.log(finalURL);
     return res.status(200).send({ url: finalURL });
   });
 };
